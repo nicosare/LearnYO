57d460a2433235df816b97ead2e803dd0733fedc
nicosare <dgalkin7@gmail.com>
2021-06-21 23:56:40 +0500

    Добавлены новые функции
    Обновлены фоны в меню и в уровнях,
    Изменены шрифты, подкорректирован текст,
    Добавлена энциклопедия,
    Добавлена новая мутация на первом уровне - иммунитет,
    Добавлен логотип в главное меню и в превью

--- Learn your organism(alpha)/Assets/Scripts/Dead.cs
+++ Learn your organism(alpha)/Assets/Scripts/Dead.cs
@@ -6,23 +6,37 @@ public class Dead : MonoBehaviour
 {
     public GameObject panelLost;
     public GameObject joystic;
+    public GameObject shield;
+
+    private bool IsProtect()
+    {
+        return shield.activeSelf;
+    }
 
     private void OnCollisionEnter2D(Collision2D collisison)
     {
         if (collisison.gameObject.CompareTag("Enemy"))
         {
-            if (GameObject.FindGameObjectsWithTag("Player").Length == 1)
+            if (!IsProtect())
             {
-                panelLost.SetActive(true);
-                Time.timeScale = 0;
-                joystic.SetActive(false);
+                if (GameObject.FindGameObjectsWithTag("Player").Length == 1)
+                {
+                    panelLost.SetActive(true);
+                    Time.timeScale = 0;
+                    joystic.SetActive(false);
+                }
+                else
+                {
+                    gameObject.SetActive(false);
+                    if (!gameObject.activeSelf)
+                        Camera2DFollowTDS.FindPlayer();
+                    Destroy(gameObject, 1);
+                }
             }
             else
             {
-                gameObject.SetActive(false);
-                if (!gameObject.activeSelf)
-                    Camera2DFollowTDS.FindPlayer();
-                Destroy(gameObject,1);
+                Destroy(collisison.gameObject);
+                shield.SetActive(false);
             }
         }
     }
--- Learn your organism(alpha)/Assets/Scripts/Score.cs
+++ Learn your organism(alpha)/Assets/Scripts/Score.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public GameObject shield;
     public GameObject scoreText;
     private int score;
     private int target;
@@ -13,7 +14,9 @@ public class Score : MonoBehaviour
     public GameObject notification;
     private int firstMutation;
     private int secondMutation;
+    private int thirdMutation;
     private bool isSelected;
+    private int rndMutation;
 
     void Start()
     {
@@ -26,13 +29,14 @@ public class Score : MonoBehaviour
     {
         if (!isSelected)
         {
-            firstMutation = Random.Range(0, 7);
-            secondMutation = Random.Range(7, 15);
+            firstMutation = Random.Range(0, 5);
+            secondMutation = Random.Range(5, 10);
+            thirdMutation = Random.Range(10, 15);
         }
         if (collisison.gameObject.CompareTag("Point"))
         {
             Count();
-            Mutation(firstMutation, secondMutation);
+            Mutation(firstMutation, secondMutation, thirdMutation);
             isSelected = true;
         }
     }
@@ -50,9 +54,9 @@ public class Score : MonoBehaviour
         }
     }
 
-    public void Mutation(int firstMutation, int secondMutation)
+    public void Mutation(int firstMutation, int secondMutation, int thirdMutation)
     {
-        if (score == firstMutation || score == secondMutation)
+        if (score == firstMutation || score == secondMutation || score == thirdMutation)
         {
             SetMutation();
             StartCoroutine(onWaiterOffNotification());
@@ -61,14 +65,22 @@ public class Score : MonoBehaviour
 
     public void SetMutation()
     {
-        var rndMutation = Random.Range(0, 2);
+        if (shield.activeSelf)
+            rndMutation = Random.Range(0, 2);
+        else
+            rndMutation = Random.Range(0, 3);
+
         if (rndMutation == 0)
         {
             this.GetComponent<PlayerMove>().Speed *= 1.3f;
         }
-        else
+        if (rndMutation == 1)
         {
             this.GetComponent<Transform>().localScale *= 1.3f;
+        }
+        if (rndMutation == 2)
+        {
+            shield.SetActive(true);
         }
     }
 
